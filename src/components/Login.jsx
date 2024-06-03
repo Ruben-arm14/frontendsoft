c3e3962809ae954744e5a54fd53f21ddfd32e296
ruben-arm14 <rubenqc_1405@outlook.com>
2024-06-03 12:13:48 -0500

    cambios

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,55 +1,43 @@
 import React, { useState } from 'react';
-import logo from '../logo.png';
+import './login.css'; // Importa tu archivo CSS
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    // Validación simple de campos (puedes agregar más validaciones)
-    if (!email || !password) {
-      setError('Por favor, completa todos los campos.');
-      return;
-    }
-
-    // Aquí iría la lógica para enviar los datos al backend y autenticar al usuario
-    // ...
-
-    // Ejemplo de redirección exitosa (debes implementar la lógica real)
-    window.location.href = '/resultados';
+    // Aquí iría tu lógica de inicio de sesión
   };
 
   return (
-    <form onSubmit={handleSubmit} className="login-form">
-      <img src={logo} alt="Logo de la aplicación" className="logo" style={{ marginBottom: '20px' }} />
-      {error && <p className="error-message">{error}</p>}
-      <div className="form-group">
-        <label htmlFor="email">Correo</label>
+    <div className="login-container">
+      <img src="logo.png" alt="Logo de Lumen Investiga" />
+      <h2>LUMENINVESTIGA</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Correo:</label>
         <input
           type="email"
           id="email"
+          name="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          required
         />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Contraseña</label>
+
+        <label htmlFor="password">Contraseña:</label>
         <input
           type="password"
           id="password"
+          name="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          required
         />
-      </div>
-      <button type="submit">Ingresar</button>
-      <button type="button">Crear cuenta</button>
-    </form>
-);
+
+        <button type="submit">Ingresar</button>
+      </form>
+      <a href="/register">Crear cuenta</a>
+    </div>
+  );
 }
 
 export default Login;
